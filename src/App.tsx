4a134edfb45784be5ebd0997d7a53609b2a0cfe2
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2022-06-04 12:47:52 -0700

    Yee

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useConnectWallet, useSetChain, useWallets } from '@web3-onboard/react'
+import { useConnectWallet } from '@web3-onboard/react'
 import 'easymde/dist/easymde.min.css'
 import { ethers } from 'ethers'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import SimpleMDE from 'react-simplemde-editor'
 import { Layout } from './components/Layout'
 import { getEthersContracts } from './contractBooter'
 import { initWeb3Onboard } from './onboard'
-import type { Account } from './types'
 
 // Must be called outside of the App function
 initWeb3Onboard()
@@ -14,29 +13,10 @@ initWeb3Onboard()
 function App() {
   const [
     {
-      wallet, // the wallet that has been connected or null if not yet connected
-      connecting // boolean indicating if connection is in progress
-    },
-    connect, // function to call to initiate user to connect wallet
-    disconnect // function to call to with wallet<DisconnectOptions> to disconnect wallet
+      wallet // the wallet that has been connected or null if not yet connected
+    }
   ] = useConnectWallet()
 
-  const [
-    {
-      chains, // the list of chains that web3-onboard was initialized with
-      connectedChain, // the current chain the user's wallet is connected to
-      settingChain // boolean indicating if the chain is in the process of being set
-    },
-    setChain // function to call to initiate user to switch chains in their wallet
-  ] = useSetChain()
-
-  // This hook allows you to track the state of all the currently connected wallets
-  const connectedWallets = useWallets()
-
-  // The user's currently connected account
-  const [account, setAccount] = useState<Account | null>(null)
-
-  // The address to send to
   const [title, setTitle] = useState('')
   const [page, setPageRoute] = useState(1)
 
@@ -46,42 +26,8 @@ function App() {
 
   const onChange = (value: string) => setValue(value)
 
-  useEffect(() => {
-    // If `wallet` is defined then the user is connected
-    if (wallet) {
-      const { name, avatar } = wallet?.accounts[0].ens ?? {}
-      setAccount({
-        address: wallet.accounts[0].address,
-        balance: wallet.accounts[0].balance,
-        ens: { name, avatar: avatar?.url }
-      })
-    }
-  }, [wallet])
-
-  useEffect(() => {
-    if (wallet?.provider) {
-      setProvider(
-        new ethers.providers.Web3Provider(wallet.provider, {
-          name: 'dev',
-          chainId: 31337
-        })
-      )
-    }
-  }, [wallet])
-
-  const connectWallet = () => {
-    connect({})
-  }
-
   const setPage = (page: number) => {
     setPageRoute(page)
-  }
-
-  const disconnectWallet = () => {
-    if (wallet?.label) {
-      disconnect({ label: wallet?.label })
-      setAccount(null)
-    }
   }
 
   const sendTransaction = async () => {
@@ -96,17 +42,6 @@ function App() {
 
   return (
     <Layout setPage={setPage} pageId={page}>
-      <div className="fixed w-full top-0 right-0 p-3 flex justify-end items-center">
-        {/* {wallet ? (
-          <Network
-            chains={chains}
-            connectedChain={connectedChain}
-            setChain={(chainId) => setChain({ chainId })}
-          />
-        ) : null}
-        <div className="mx-1" />
-        <AccountDetails account={account} /> */}
-      </div>
       <div className="my-32 text-3xl font-bold">hardhat-graph demo</div>
       <div className="my-32 text-3xl font-bold"> </div>
       {page == 1 ? (
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Disclosure } from '@headlessui/react'
 import { ReactNode } from 'react'
+import { Hammer } from 'tabler-icons-react'
 import { ConnectWallet } from './ConnectWallet'
 import { classNames } from './utils'
 
@@ -18,6 +19,7 @@ export function Layout({
   setPage: (page: number) => void
   pageId: number
 }) {
+  const selectedNavigation = navigation.find((nav) => nav.pageId === pageId)
   return (
     <>
       <div className="min-h-full">
@@ -25,35 +27,29 @@ export function Layout({
           <>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex justify-between h-16">
-                <div className="flex">
-                  <div className="flex-shrink-0 flex items-center">
-                    <img
-                      className="hidden block h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                    {navigation.map((item) => {
-                      const isSelected = item.pageId === pageId
-                      return (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          onClick={() => setPage(item.pageId)}
-                          className={classNames(
-                            isSelected
-                              ? 'border-indigo-500 text-gray-900'
-                              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                            'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
-                          )}
-                          aria-current={isSelected ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
-                      )
-                    })}
-                  </div>
+                <div className="flex-shrink-0 gap-2 flex items-center text-gray-900 font-bold">
+                  <Hammer /> Hardhat Graph Demo
+                </div>
+                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                  {navigation.map((item) => {
+                    const isSelected = item.pageId === pageId
+                    return (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        onClick={() => setPage(item.pageId)}
+                        className={classNames(
+                          isSelected
+                            ? 'border-indigo-500 text-gray-900'
+                            : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                          'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                        )}
+                        aria-current={isSelected ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    )
+                  })}
                 </div>
                 <div className=" sm:ml-6 sm:flex sm:items-center">
                   <ConnectWallet />
@@ -66,7 +62,9 @@ export function Layout({
         <div className="py-10">
           <header>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight text-gray-900">Dashboard</h1>
+              <h1 className="text-3xl font-bold leading-tight text-gray-900">
+                {selectedNavigation?.name}
+              </h1>
             </div>
           </header>
           <main>
